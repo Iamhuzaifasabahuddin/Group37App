9c929e932ff3d81a08c51d9fe2239b52ceb7ed22
Huzaifa Sabahuddin <hs540@student.le.ac.uk>
2024-02-06 14:35:52 +0000

    Updated filtering and css
    Updated backend to filter courses and error catching for the courses
    Updated css for login, navbar

--- Group37-Software_Engineering/src/main/java/com/example/group37software_engineering/controller/FilterController.java
+++ Group37-Software_Engineering/src/main/java/com/example/group37software_engineering/controller/FilterController.java
@@ -2,13 +2,16 @@ package com.example.group37software_engineering.controller;
 
 
 import com.example.group37software_engineering.model.Course;
+import com.example.group37software_engineering.model.MyUser;
 import com.example.group37software_engineering.repo.CourseRepository;
+import com.example.group37software_engineering.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.security.Principal;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,29 +21,41 @@ import java.util.stream.Collectors;
 public class FilterController {
 
     @Autowired
+    private UserRepository userRepository;
+    @Autowired
     private CourseRepository courseRepository;
 
     @RequestMapping(value = "/filter")
-    public String getFilter(@RequestParam String category, Model model){
-        model.addAttribute("Filtered", courseRepository.findAllByCategory(category));
-        return "Course/filter";
+    public String getFilter(@RequestParam String category, Model model, Principal principal) {
+        MyUser user = userRepository.findByUsername(principal.getName());
+        model.addAttribute("user", user);
+        model.addAttribute("courseList", courseRepository.findAllByCategory(category));
+        return "Course/courses";
 
     }
 
     @RequestMapping(value = "/search")
-    public String getSearch(@RequestParam String searchTerm, Model model) {
-        model.addAttribute("course", courseRepository.findCoursesByTitle(searchTerm));
-        return "Course/search";
+    public String getSearch(@RequestParam String searchTerm, Model model, Principal principal) {
+        MyUser user = userRepository.findByUsername(principal.getName());
+        model.addAttribute("user", user);
+        if(searchTerm != null && !searchTerm.isBlank()){
+            model.addAttribute("course", courseRepository.findCoursesByTitle(searchTerm));
+
+        }
+        model.addAttribute("Courseerror", "No such course found!");
+        return "Course/courses";
     }
 
     @RequestMapping(value = "/duration")
-    public String getDuration(@RequestParam double duration, Model model) {
+    public String getDuration(@RequestParam double duration, Model model, Principal principal) {
         List<Course> sortedCourses = courseRepository.findAllByDurationGreaterThanEqual(duration)
                 .stream()
                 .sorted(Comparator.comparingDouble(Course::getDuration))
                 .toList();
-        model.addAttribute("CourseList",sortedCourses);
-        return "Course/duration";
+        MyUser user = userRepository.findByUsername(principal.getName());
+        model.addAttribute("user", user);
+        model.addAttribute("courseList",sortedCourses);
+        return "Course/courses";
     }
 
 }
